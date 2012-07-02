d16885a98f921b7a05777bdec4a41b31db200e74
Ronald.Hordijk@gmail.com <Ronald.Hordijk@gmail.com@70bf06fc-261b-11df-999e-776db95dd6c3>
2012-07-02 17:09:44 +0000

    Get rid of ext for now initiate the animation loop

--- js/main.js
+++ js/main.js
@@ -1,26 +1,50 @@
 /*jslint browser: true, windows: true, es5: true, white: true, nomen: false, plusplus: false, maxerr: 500, indent: 2*/
 /*global Ext: false, window: false, hidato: false */
 
-Ext.onReady(function () {
-  'use strict';
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
-  function initialize() {
-    var
-      canvasDiv = document.getElementById('canvasdiv'),
-      canvas,
-      context;
-      
-    canvas = document.createElement('canvas');
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-    canvasDiv.appendChild(canvas);
-     
-    hidato.board.initialize(hidato.data);
-    hidato.path.initialize(hidato.board);
-    hidato.drawer.initialize(canvas);
+function animate() {
+  requestAnimFrame(animate);
+  hidato.drawer.draw(hidato.board);
+}
+
+function initialize() {
+  var
+    canvasDiv = document.getElementById('canvasdiv'),
+    canvas,
+    context;
     
-    hidato.drawer.draw(hidato.board);
-  }
+  canvas = document.createElement('canvas');
+  canvas.setAttribute('width', window.innerWidth);
+  canvas.setAttribute('height', window.innerHeight);
+  canvasDiv.appendChild(canvas);
+   
+  hidato.board.initialize(hidato.data);
+  hidato.path.initialize(hidato.board);
+  hidato.drawer.initialize(canvas);
 
-  initialize();
-});
+  animate(); 
+}
+
+function onDeviceReady() {
+  initialize(); 
+}
+
+function onLoad() {
+//  if (cordova !== undefined) {
+//    document.addEventListener("deviceready", onDeviceReady, false);
+//  } else {
+    initialize(); 
+//  }
+}
+
+    
