437d0b32ad813b1a48b5c701ea8c4d190bf1ba8e
Ronald.Hordijk@gmail.com <Ronald.Hordijk@gmail.com@70bf06fc-261b-11df-999e-776db95dd6c3>
2012-07-04 17:40:11 +0000

    initial path logic

--- trunk/js/path.js
+++ trunk/js/path.js
@@ -1,21 +1,147 @@
-/*jslint browser: true, windows: true, es5: true, white: true, nomen: false, plusplus: false, maxerr: 500, indent: 2*/
+/*jslint browser: true, windows: true, es5: true, nomen: false, plusplus: false, maxerr: 500, indent: 2*/
 /*global hidato: false */
 
 hidato.path = (function () {
   'use strict';
 
   var
+    activeCell_,
+    direction_ = 'up',
+    nrCells_ = 0,
+    startHole_,
+    endHole_,
     result = {
       path: []
-    };  
+    };
+
+  function flipDirection() {
+    if (direction_ === 'up') {
+      direction_ = 'down';
+    } else {
+      direction_ = 'up';
+    }
+  }
+
+  function allFilled(cell) {
+    var
+      empty = false,
+      i;
+
+    for (i = 0; i < nrCells_; i++) {
+      if (!result.path[i]) {
+        empty = true;
+        break;
+      }
+    }
+
+    return !empty;
+  }
+
+  function findHole(cell) {
+    var
+      i;
+
+    startHole_ = -1;
+
+    if (direction_ === 'up') {
+      for (i = cell.sol; i < nrCells_; i++) {
+        if (!result.path[i]) {
+          startHole_ = i;
+          break;
+        }
+      }
+    } else {
+      for (i = cell.sol; i > 0; i--) {
+        if (!result.path[i]) {
+          startHole_ = i;
+          break;
+        }
+      }
+    }
+
+    // no hole go the othe direction
+    if (startHole_ < 0) {
+      flipDirection();
+      findHole(cell);
+    }
+
+    // find end;    
+    if (direction_ === 'up') {
+      for (i = startHole_; i < nrCells_; i++) {
+        if (result.path[i]) {
+          endHole_ = i;
+          break;
+        }
+      }
+    } else {
+      for (i = startHole_; i > 0; i--) {
+        if (result.path[i]) {
+          endHole_ = i;
+          break;
+        }
+      }
+    }
+  }
+
+  result.select = function (cell) {
+
+    if (cell.type === 'none') {
+      return;
+    }
+
+    if (cell.type === 'fixed') {
+      if (cell === activeCell_) {
+        flipDirection();
+      }
+
+      if (!allFilled()) {
+        findHole(cell);
+      }
+    } else if (cell.type === 'open') {
+      cell.type = 'used';
+      cell.val = startHole_;
+      result.path[startHole_] = cell;
+
+      startHole_ = (direction_ === 'up') ? startHole_ + 1 : startHole_ - 1;
+
+      if (startHole_ === endHole_) {
+        if (!allFilled()) {
+          startHole_ = -1;
+          endHole_ = -1;
+        } else {
+          findHole(cell);
+        }
+      }
+    } else if (cell.type === 'open') {
+      cell.type = 'open';
+      result.path[cell.val] = undefined;
+      startHole_ = cell.val;
+      endHole_ = (direction_ === 'up') ? startHole_ + 1 : startHole_ - 1;
+    }
+
+    activeCell_ = cell;
+  };
 
   result.initialize = function (board) {
     board.cells.forEach(function (cell) {
-      if (cell.sol > 0 ){
-        result.path[cell.sol] = cell;           
+      if (cell.type === 'fixed') {
+        result.path[cell.sol] = cell;
       }
     });
+
+    if (result.path[0]) {
+      result.path[0].special = 'start';
+    }
+
+    nrCells_ = result.path.length;
+    if (result.path[nrCells_ - 1]) {
+      result.path[nrCells_ - 1].special = 'end';
+    }
+
+    if (result.path[0]) {
+      result.select(result.path[0]);
+    }
   };
 
   return result;
-} ());
+}());
