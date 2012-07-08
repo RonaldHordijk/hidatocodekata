188a6e53d492c2605072befc27b9641a9a342bdf
Ronald.Hordijk@gmail.com <Ronald.Hordijk@gmail.com@70bf06fc-261b-11df-999e-776db95dd6c3>
2012-07-08 15:49:00 +0000

    initial animation stuff

---
+++ trunk/js/animate.js
@@ -0,0 +1,71 @@
+/*jslint browser: true, windows: true, es5: true, nomen: false, plusplus: false, maxerr: 500, indent: 2*/
+/*global hidato: false */
+
+hidato.createStartAnimation = function (cell) {
+  'use strict';
+
+  var
+    context_,
+    coordCellConverter_,
+    cell_ = cell,
+    startTime_,
+    result = {};
+
+  result.isInitialized = function () {
+    return context_ !== undefined;
+  };
+
+  result.initialize = function (context, coordCellConverter, startTime) {
+    context_ = context;
+    coordCellConverter_ = coordCellConverter;
+    startTime_ = startTime;
+  };
+
+  result.getTimeStep = function (time) {
+    return (0.0001 * time) % 1;
+  };
+
+  result.animate = function (time) {
+    var
+      NRCIRCLES = 10,
+      i, r,
+      radius,
+      center = {},
+      rect = coordCellConverter_.celltoRect(cell_),
+      timestep = result.getTimeStep(time);
+
+    center.x = 0.5 * rect.x1 + 0.5 * rect.x2;
+    center.y = 0.5 * rect.y1 + 0.5 * rect.y2;
+    radius = 0.7 * (rect.x2 - rect.x1);
+
+    context_.lineWidth = 0.5 * radius / NRCIRCLES;
+
+    for (i = 0; i < NRCIRCLES; i++) {
+      r = (i / NRCIRCLES + timestep) % 1;
+      context_.strokeStyle = 'rgba(200,255,100,' + (1 - r) + ')';
+      context_.beginPath();
+      context_.arc(center.x, center.y, r * radius, 0, 2 * Math.PI, true);
+      context_.stroke();
+    }
+
+  };
+
+  result.isFinished = function (time) {
+    return false;
+  };
+
+  return result;
+};
+
+hidato.createEndAnimation = function (cell) {
+  'use strict';
+
+  var
+    result = hidato.createStartAnimation(cell);
+
+  result.getTimeStep = function (time) {
+    return (-0.0001 * time) % 1 + 1;
+  };
+
+  return result;
+};
