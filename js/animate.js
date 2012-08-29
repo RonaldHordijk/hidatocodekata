27996c6ad39993dead35a37cb40aaf193a7e62af
Ronald.Hordijk@gmail.com <Ronald.Hordijk@gmail.com@70bf06fc-261b-11df-999e-776db95dd6c3>
2012-08-29 13:13:15 +0000

    Add finished animation

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,4 @@
-/*jslint browser: true, windows: true, es5: true, nomen: false, plusplus: false, maxerr: 500, indent: 2*/
+/*jslint browser: true, windows: true, es5: true, nomen: true, plusplus: true, maxerr: 500, indent: 2*/
 /*global hidato: false */
 
 hidato.createStartAnimation = function (cell) {
@@ -47,13 +47,14 @@ hidato.createStartAnimation = function (cell) {
     center.y = 0.5 * rect.y1 + 0.5 * rect.y2;
     radius = 0.7 * (rect.x2 - rect.x1);
 
-    context_.lineWidth = context_.lineWidth = Math.max(1, 0.25 * radius / NRCIRCLES);
+    context_.lineWidth = Math.max(1, 0.25 * radius / NRCIRCLES);
 
     for (i = 0; i < NRCIRCLES; i++) {
       r = (i / NRCIRCLES + timestep) % 1;
       context_.strokeStyle = result.getColor(1 - r);
+//      context_.strokeStyle = result.getColor(1);
       context_.beginPath();
-      context_.arc(center.x, center.y, r * radius, 0, 2 * Math.PI, true);
+      context_.arc(center.x, center.y, r * radius, 0, 2 * Math.PI, false);
       context_.stroke();
     }
 
@@ -186,8 +187,8 @@ hidato.createSelectAnimation = function (cell) {
 
     center.x = 0.5 * rect.x1 + 0.5 * rect.x2;
     center.y = 0.5 * rect.y1 + 0.5 * rect.y2;
-    radius = 0.5 * (1 + 0.007 * (time - startTime_)) * (rect.x2 - rect.x1);
-    alpha = 1 - 0.004 * (time - startTime_);
+    radius = 0.5 * (1 + 0.0035 * (time - startTime_)) * (rect.x2 - rect.x1);
+    alpha = 1 - 0.003 * (time - startTime_);
 
     context_.lineWidth = 2;
     context_.strokeStyle = 'rgba(200,200,255,' + alpha + ')';
@@ -200,6 +201,102 @@ hidato.createSelectAnimation = function (cell) {
   result.isFinished = function (time) {
     return (time - startTime_) > 1000;
   };
+
+  return result;
+};
+
+hidato.createFinishedAnimation = function (cells) {
+  'use strict';
+
+  var
+    context_,
+    coordCellConverter_,
+    drawingScheme_,
+    cells_ = cells,
+    startTime_,
+    minPos_,
+    maxPos_,
+    result = {};
+
+  function calcMinMaxPos() {
+    var 
+      i, cell;
+      
+    minPos_ = 100;
+    maxPos_ = 0;  
+    for(i = 1; i < cells.length; i++) {
+      cell = cells_[i].cell || cells_[i];
+      
+      minPos_ = Math.min(minPos_, cell.x - cell.y); 
+      maxPos_ = Math.max(maxPos_, cell.x - cell.y); 
+    }  
+    
+  }
+
+
+  result.isInitialized = function () {
+    return context_ !== undefined;
+  };
+
+  result.initialize = function (context, drawingScheme, startTime) {
+    context_ = context;
+    drawingScheme_ = drawingScheme;
+    startTime_ = startTime;
+    
+    
+  };
+
+  result.animate = function (coordCellConverter, time) {
+    var
+      i,
+      cell, 
+      pos,
+      t,
+      radius,
+      center = {},
+      rect;
+
+    coordCellConverter_ = coordCellConverter;
+
+    t = (0.0003 * (time - startTime_) * (maxPos_ - minPos_)) % (maxPos_ - minPos_);
+    
+    for(i = 1; i < cells.length; i++) {
+      cell = cells_[i].cell || cells_[i];
+      
+      pos = cell.x - cell.y;
+      pos = pos - minPos_;
+      
+      if (Math.abs(t - pos) < 1.5) {
+
+        rect = coordCellConverter_.celltoRect(cell);
+    
+        context_.fillStyle = 'rgba(128,128,255,0.4)';
+        context_.beginPath();
+
+        center.x = 0.5 * rect.x1 + 0.5 * rect.x2;
+        center.y = 0.5 * rect.y1 + 0.5 * rect.y2;
+        center.w = 0.5 * (rect.x2 - rect.x1);
+        center.h = 0.5 * (rect.x2 - rect.x1);
+        
+        rect.x1 = center.x - Math.min(1, 1.5 - Math.abs(t - pos)) * center.w;  
+        rect.x2 = center.x + Math.min(1, 1.5 - Math.abs(t - pos)) * center.w;  
+        rect.y1 = center.y - Math.min(1, 1.5 - Math.abs(t - pos)) * center.h;  
+        rect.y2 = center.y + Math.min(1, 1.5 - Math.abs(t - pos)) * center.h;  
+
+        context_.rect(rect.x1, rect.y1, rect.x2 - rect.x1, rect.y2 - rect.y1);
+        context_.fill();
+        
+      }
+      
+    }  
+
+  };
+
+  result.isFinished = function (time) {
+    return false;
+  };
+
+  calcMinMaxPos();
 
   return result;
 };
